a2f1fb8b7ce97488b7ed1994f0059a677e9a9a7b
IoannisTselekoglou <janni.tselekoglou@googelmail.com>
2022-12-05 20:18:37 +0100

    merged p1/p2 in problem

--- adv4/4.py
+++ adv4/4.py
@@ -8,38 +8,25 @@ def input():
     return reader
 
 def p1():
-    ans = 0
+    ans_1 = 0 ; ans_2 = 0
     reader = input()
     for row in reader:
         a, b = row[0].split("-")
         c, d = row[1].split("-")
-
         l1 = [i for i in range(x(a),x(b)+1)]
         l2 = [i for i in range(x(c),x(d)+1)]
 
         if set(l1).issubset(l2) or set(l2).issubset(l1):
-           ans += 1
+           ans_1 += 1
 
-    return ans
+        #part 2
+        for i in l1:
+          if i in l2:
+            ans_2 +=1
+            break
+    return ans_1, ans_2
 
-#print(p1())
+
+print(p1())
 
 
-def p2():
-    ans = 0
-    reader = input()
-    for row in reader:
-       a, b = row[0].split("-")
-       c, d = row[1].split("-")
-
-       l1 = [i for i in range(x(a),x(b)+1)]
-       l2 = [i for i in range(x(c),x(d)+1)]
-
-       for i in l1:
-           if i in l2:
-               ans += 1
-               break
-
-    return ans
-
-print(p2())
